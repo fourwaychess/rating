aa96adc2c234906fd50d25e60ecb861227da3313
Aujicini <55375045+Aujicini@users.noreply.github.com>
2021-01-03 02:26:15 -0500

    Add calculator test class.
    Backtrack to this.

---
+++ tests/CalculatorTest.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+namespace FourWayChess\Tests;
+
+use FourWayChess\Rating\RatingCalculator;
+use PHPUnit\Framework\TestCase;
+
+class CalculatorTest extends TestCase
+{
+    /**
+     * @return void Returns nothing.
+     */
+    public function testCalculator(): void
+    {
+        $this->assertTrue(true);
+    }
+}
