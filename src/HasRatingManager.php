2e6e8e72b33adb12a3b7c79555d06c9f6241a2cc
Aujicini <55375045+Aujicini@users.noreply.github.com>
2021-01-03 15:59:11 -0500

    ensure the calculator is statically called.

--- src/HasRatingManager.php
+++ src/HasRatingManager.php
@@ -12,12 +12,12 @@ trait HasRatingManager
     public function calculateRatings()
     {
         $result = $this->result;
-        $rr = (new RatingCalculator)->calculate(($this->blue + $this->green) / 2, $this->red, $this->r_constant, $result);
-        $yr = (new RatingCalculator)->calculate(($this->blue + $this->green) / 2, $this->yellow, $this->y_constant, $result);
+        $rr = RatingCalculator::calculate(($this->blue + $this->green) / 2, $this->red, $this->r_constant, $result);
+        $yr = RatingCalculator::calculate(($this->blue + $this->green) / 2, $this->yellow, $this->y_constant, $result);
         if ($result === -1)
             $result = 1;
-        $br = (new RatingCalculator)->calculate(($this->red + $this->yellow) / 2, $this->blue, $this->b_constant, $result);
-        $gr = (new RatingCalculator)->calculate(($this->red + $this->yellow) / 2, $this->green, $this->g_constant, $result);
+        $br = RatingCalculator::calculate(($this->red + $this->yellow) / 2, $this->blue, $this->b_constant, $result);
+        $gr = RatingCalculator::calculate(($this->red + $this->yellow) / 2, $this->green, $this->g_constant, $result);
         return [$rr, $br, $yr, $gr];
     }
 }
