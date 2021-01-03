9982519f16a2d0bd23331da63eead389e49df6c7
Aujicini <55375045+Aujicini@users.noreply.github.com>
2021-01-03 01:58:15 -0500

    Add rating calculator.

--- src/HasRatings.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace FourWayChess\Rating;
-
-trait HasRatings
-{
-}
---
+++ src/RatingCalculator.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace FourWayChess\Rating;
+
+class RatingCalculator
+{
+    /**
+     * Calculate the new ratings.
+     *
+     * @param int $team     Team A's rating.
+     * @param int $user     User's rating.
+     * @param int $constant A constant var.
+     * @param int $result   Did the user win.
+     *
+     * @return array Returns the updated ratings.
+     */
+    public static function calculate(int $team, int $user, int $constant, int $result): array
+    {
+        $userProbability = (1.0 * 1.0 / (1 + 1.0 * pow(10, 1.0 * ($user - $team) / 400)))
+        if ($result === 1)
+            $user = $user + $constant * (1 - $userProbability);
+        else
+            $user = $user + $constant * (0 - $userProbability);
+        return $user;
+    }
+}
