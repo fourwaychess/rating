904022c1396f8616d414ce6b27787b4e523b6839
Aujicini <55375045+Aujicini@users.noreply.github.com>
2021-01-03 14:48:55 -0500

    Add the rating config.

---
+++ config/rating.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Rating
+    |--------------------------------------------------------------------------
+    |
+    | This option allows you to set the defualt rating for each variant in
+    | Four Way Chess upon registration.
+    |
+    */
+
+    'defualt' => floatval(1200)
+
+]
