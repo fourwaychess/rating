89060466fcbbcbc791e2b052f044e846ce86b868
Aujicini <55375045+Aujicini@users.noreply.github.com>
2021-01-02 23:25:20 -0500

    Add a static Rating class containing static info.

---
+++ src/Rating.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Laravel\Cashier;
+
+class Rating
+{
+    /** @var bool $runsMigrations Indicates if Rating migrations will be run. */
+    public static $runsMigrations = true;
+}
